ef8ed2b07eeb757c25f74eb55afd72a16339dfec
brunoCesarOliveira <bruno.oliveira@aliare.co>
2023-01-28 10:31:39 -0300

    Delete Seller

---
+++ SalesWebMvc/Service/DeparmentService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Service
+{
+    public class DeparmentService
+    {
+
+        private readonly SalesWebMvcContext _context;
+        public DeparmentService(SalesWebMvcContext context)
+        {
+            _context=context;   
+        }
+
+        public List<Department> FindAll()
+        {
+           return _context.Department.OrderBy( x=>x.Name).ToList();   
+        }
+    }
+}
