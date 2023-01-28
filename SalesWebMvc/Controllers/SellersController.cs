4fe90a1c86a5a6d0da6a3593ef8ec92604884345
brunoCesarOliveira <bruno.oliveira@aliare.co>
2023-01-28 10:22:44 -0300

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -39,5 +39,28 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
     
+            
+        public IActionResult Delete (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value) ; // Pegar o o valor por ele ser nulo precisa do .Value
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
